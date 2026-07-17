52340d8b936306f86a76907b61fbaba1ed8dd899
Nikita Edel <nikita.edel.dev@gmail.com>
2026-07-17 19:08:26 +0200

    give up trying to use c23

--- include/boost/preprocessor/variadic/detail/has_opt.h
+++ include/boost/preprocessor/variadic/detail/has_opt.h
@@ -14,7 +14,7 @@
 #
 # include <boost/preprocessor/config/config.h>
 #
-# if defined(__cplusplus) && __cplusplus > 201703L || (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L)
+# if defined(__cplusplus) && __cplusplus > 201703L
 #
 # if BOOST_PP_VARIADICS_MSVC
 # include <boost/preprocessor/cat.h>
